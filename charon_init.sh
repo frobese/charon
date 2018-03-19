4d0b59e2e74df0e2b25e6d43eb0cdb779753bc66
Hans Bernhard Gödeke <hgoedeke@frobese.de>
2018-03-19 16:50:22 +0100

    1.6.1f script fixes

--- charon_init.sh
+++ charon_init.sh
@@ -20,11 +20,12 @@ DEVS="$(blkid /dev/sd* | awk '{ match($0,/^\/dev\/sd.[1-9]/) ; print substr($0, 
 
 (ls /mnt/usb || mkdir /mnt/usb)
 
+FILES=("charon.cfg" "charon_signature.txt")
 for DEV in $DEVS; do
 	mount $DEV /mnt/usb 
 	if [ $? -eq 0 ]; then
-        for FILE in ["charon.cfg", "charon_signature.txt"];
-            cp "/mnt/usb/$FILE" "/root/$FILE"
+        for FILE in "${FILES[@]}"; do 
+            cp /mnt/usb/$FILE /root/$FILE
         done
         cp /var/log/charon.* $DEV
     fi
